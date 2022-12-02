628f11300c07c69e9dbd2b4e247be9969c7116e9
AndreaGualandris <andrea.gualandris@usi.ch>
2022-12-02 15:05:17 +0100

    urlImage, urlHouse in Rentcafe and Apartamentfinder

--- Spiders/apfinder.com/appFinder.py
+++ Spiders/apfinder.com/appFinder.py
@@ -1,6 +1,9 @@
 #scrape link https://www.apartmentfinder.com/Short-Term-Housing/Illinois/Chicago
 # list of apartaments layout-row layout-wrap
 
+
+# flex-12 ellipses layout-hidden-xs desktop-title a::attr(href)      url pagina casa
+
 import scrapy
 from scrapy.selector import Selector
 
@@ -12,28 +15,6 @@ class SpiderApartamentFinder(scrapy.Spider):
     def parse(self, response):
         self.i+=1
         for h in response.css("#listingContainer ol"):
-            # for houses in h.css(".infoContainer.propertyInfo.layout-row.layout-wrap.layout-align-space-between-stretch.layout-align-gt-xs-start-stretch"):
-            # for houses in h.css("div.imageContainer.media"):   
-            # if h.css(".level5.layout-row.layout-wrap.adLevel5"):
-            #     for houses in h.css(".level5.layout-row.layout-wrap.adLevel5"):
-            #         yield {
-            #             # "name": houses.css(".infoContainer.propertyInfo.layout-row.layout-wrap.layout-align-space-between-stretch.layout-align-gt-xs-start-stretch h3 a::text").get(),
-            #             "title": houses.css(".flex-12.ellipses.layout-hidden-xs.desktop-title span::text").get().strip(),
-            #             "address": houses.css(".flex-12.address.ellipses::text").get().strip(),
-            #             "price": houses.css(".altRentDisplay.layout-hidden-xs::text").get().strip(),
-            #             "beds": houses.css(".unitLabel::text").get().strip(),
-            #             "urlImage": houses.css(".carouselInner div::attr(data-image-url)").get()
-            #         }
-            # elif (h.css(".level6.layout-row.layout-wrap.adLevel6")):
-            #     for houses in h.css(".level6.layout-row.layout-wrap.adLevel6"):
-            #         yield {
-            #             # "name": houses.css(".infoContainer.propertyInfo.layout-row.layout-wrap.layout-align-space-between-stretch.layout-align-gt-xs-start-stretch h3 a::text").get(),
-            #             "title": houses.css(".flex-12.ellipses.layout-hidden-xs.desktop-title span::text").get().strip(),
-            #             "address": houses.css(".flex-12.address.ellipses::text").get().strip(),
-            #             "price": houses.css(".altRentDisplay.layout-hidden-xs::text").get().strip(),
-            #             "beds": houses.css(".unitLabel::text").get().strip(),
-            #             "urlImage": houses.css(".carouselInner div::attr(data-image-url)").get()
-            #         }
             for houses in h.css("li article"):
                 yield {
                         # "name": houses.css(".infoContainer.propertyInfo.layout-row.layout-wrap.layout-align-space-between-stretch.layout-align-gt-xs-start-stretch h3 a::text").get(),
@@ -41,7 +22,8 @@ class SpiderApartamentFinder(scrapy.Spider):
                         "address": houses.css(".flex-12.address.ellipses::text").get().strip(),
                         "price": houses.css(".altRentDisplay.layout-hidden-xs::text").get().strip(),
                         "beds": houses.css(".unitLabel::text").get().strip(),
-                        # "urlImage": houses.css(".carouselInner div::attr(data-image-url)").get()
+                        "urlImage": houses.css(".carouselInner div::attr(data-image-url)").get(),
+                        "urlHouse": houses.css(".flex-12.ellipses.layout-hidden-xs.desktop-title a::attr(href)").get()
                     }
 
             
